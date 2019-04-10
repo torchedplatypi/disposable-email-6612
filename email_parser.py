3e456298b869cd3b2b2b200315923950b4ad039f
nsolomon7 <36146549+nsolomon7@users.noreply.github.com>
2019-04-09 21:30:18 -0400

    Add files via upload
    Changes to work on every csv file and create directories to separate training data. More general working script

--- email_parser.py
+++ email_parser.py
@@ -10,13 +10,14 @@ import os
 import glob
 import re 
 
-#this script is used on one csv file at a time. Optimization will be the least of my worries at the moment
-#grab the csv file to parse
-csv = sys.argv[1]
-#get current working directory to move files to training_set
-cwd = os.getcwd()
-#remove the csv part of the file we are looking for
-fn = re.sub('\.csv', '', csv)
+#This script runs on every csv file now
+
+#grab current working directory and list of csv files
+cwd = os.getcwd() 
+filelist = [i for i in glob.glob('*.{}'.format('csv'))]
+
+
+#setups for text file names and directory names
 extension = ".txt"
 holder = "email_"
 linecount = 0 #number of emails per csv tracker
@@ -29,8 +30,12 @@ senders = cwd + slash + senders #file containing every sender instance
 subjects = cwd + slash + subjects #file containing every subject line instance
 words = cwd + slash + words #file containing a body of words for email content
 
-post = cwd + slash + training + slash + fn #directory to put each individual set of emails in
-os.mkdir(post)
+#checking to see if a training data folder exists
+trainingd = cwd + slash + training
+if os.path.isdir(trainingd):
+    trainingd = trainingd
+else:
+    os.mkdir(trainingd)
 
 #does the file exist
 if os.path.isfile(senders):
@@ -43,36 +48,42 @@ else:
    fsubj = open(subjects,"w")
    fwords = open(words,"w")
    
-#looks like each line is an email
-
-with open(csv) as file:
-    for line in file:
-        linecount += 1
-        num = str(linecount)
-        #remove excess whitespace
-        line = re.sub(' +', ' ',line)
-        #create new file in txt
-        fnf = holder + num + extension
-        outfile = open(fnf,"w")
-        outfile.write(line)
-        outfile.close()
-        #get old filepath
-        pre = cwd + slash + fnf
-        #print(pre)
-        #create new filepath   
-        #print(post)
-        post = cwd + slash + training + slash + fn + slash + fnf
-        #print(post)
-        #rename file to move to training folder
-        os.rename(pre,post)
-        sentfrom = line.split(';')[0] #grab sender
-        subjectline = line.split(';')[1]  #grab subject
-        line = line.replace(sentfrom + ';' ,"")
-        body = line.replace(subjectline + ';',"") #parse for body content
-        fsend.write(sentfrom + '\n')
-        fsubj.write(subjectline + '\n')
-        fwords.write(body + '\n\n' )
-
+#goes through every csv file
+for csv in filelist:
+    fn = re.sub('\.csv', '', csv)
+    post = cwd + slash + training + slash + fn #directory to put each individual set of emails in
+    os.mkdir(post)
+    #looks like each line is an email
+    with open(csv) as file:
+        for line in file:
+            linecount += 1
+            num = str(linecount)
+            #remove excess whitespace
+            line = re.sub(' +', ' ',line)
+            #create new file in txt
+            fnf = holder + num + extension
+            outfile = open(fnf,"w")
+            #get old filepath
+            pre = cwd + slash + fnf
+            #print(pre)
+            #create new filepath   
+            #print(post)
+            post = cwd + slash + training + slash + fn + slash + fnf
+            #print(post)
+            #rename file to move to training folder
+            os.rename(pre,post)
+            sentfrom = line.split(';')[0] #grab sender
+            subjectline = line.split(';')[1]  #grab subject
+            line = line.replace(sentfrom + ';' ,"")
+            body = line.replace(subjectline + ';',"") #parse for body content
+            fsend.write(sentfrom + '\n')
+            fsubj.write(subjectline + '\n')
+            fwords.write(body + '\n\n' )
+            outfile.write(sentfrom + '\n')
+            outfile.write(subjectline + '\n')
+            outfile.write(body + '\n')
+            outfile.close()
+        
 fsend.write('\n')
 fsubj.write('\n')
 fwords.write('\n' )     
