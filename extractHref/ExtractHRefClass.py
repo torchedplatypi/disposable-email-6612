01b66973f373d92ef170d734eefef44780b21d8f
Fei Wu <fei@lawn-143-215-112-205.lawn.gatech.edu>
2019-04-12 15:01:35 -0400

    added keywordScan to ExtractHrefClass

--- extractHref/ExtractHRefClass.py
+++ extractHref/ExtractHRefClass.py
@@ -3,18 +3,34 @@ import re
 class ExtractHRef(object):
     def __init__(self, f=""):
         self.filename=f
+        self.data_preped=False
 
     def set_filename(self, f):
         self.filename=f
 
-    def get_urls(self):
+    def prep_data(self):
+        self.data_preped = True
         fHandle = open(self.filename)
         pattern = r'(http[s]?://[^\s|^\)|^\]]+)'
-        output =[]
+        urlListAll =[]
+        keywordResult=[]
         lines = fHandle.readlines()
         fHandle.close()
         img_ext = ['.jpg','.gif','.png']
+        sensitive_list=['name', 'address', 'card', 'telephone', 'age']
+        self.raw_data = lines
         for l in lines:
+            # performing keywords scan
+            line_data = l.lower()
+            words_list = line_data.split(' ')
+            sensitive_information_flag = False
+            for key_word in sensitive_list:
+                if key_word in words_list: 
+                    sensitive_information_flag = True
+                    break
+            keywordResult.append(sensitive_information_flag)
+
+            # extracting urls in the email
             out = re.findall(pattern,l)
             urlList = []
             # urList contains the list of urls in one particular email message body
@@ -30,11 +46,27 @@ class ExtractHRef(object):
                     pass 
                 elif url not in urlList:
                     urlList.append(url)
-            output.append(urlList)
-        
-        return output
+            urlListAll.append(urlList)
+        self.urlList = urlListAll
+        self.keywordResult = keywordResult
 
+    def get_urls(self):
+        if not self.data_preped:
+            self.prep_data()
+        return self.urlList
+
+    def get_keywordsScan(self): 
+        if not self.data_preped:
+            self.prep_data()
+        return self.keywordResult
+
+    def get_raw(self): 
+        if not self.data_preped:
+            self.prep_data()
+        return self.raw_data
+    
 
 if __name__ == "__main__":
-    data = ExtractHRef("4-7-2019.csv")
+    data = ExtractHRef("data/4-5-2019.csv")
     urls = data.get_urls()
+    keywordResults = data.get_keywordsScan()
