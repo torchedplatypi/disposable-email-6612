bf6a09b4403ce2fe835ed5249c322d8e29c328bf
Fei Wu <fei@lawn-143-215-112-205.lawn.gatech.edu>
2019-04-12 13:40:15 -0400

    added word frequency parser toolkit

---
+++ toolkits/wordFreqParser.py
@@ -0,0 +1,17 @@
+import json
+filename ="testfile1.txt"
+fHandle = open(filename)
+lines = fHandle.readlines()
+fHandle.close()
+data = {}
+for l in lines[1:]:
+    l = l.strip('\n')
+    l = l.strip('()')
+    words = l.split(',')
+    key = words[0]
+    key = key.strip('u')
+    key = key.strip('\'')
+    value = float(words[1])
+    data[key] = value
+
+print (data)
